d65589343d2cd7b25d3402865c5be9646a20bd2c
vgabrielesf <vitoriagaby49@gmail.com>
2025-12-29 13:07:19 -0300

    Corrige configuração do vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/TOWING---Contratos/',
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
